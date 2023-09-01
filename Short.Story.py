6ce8500950a2e1ac3179c384731db1cdfe546e3d
Lewis Dickens <100531411+lewisad97@users.noreply.github.com>
2023-09-01 16:00:48 +0100

    Main File.

---
+++ Short.Story.py
@@ -0,0 +1,20 @@
+main_character_name = "Frodo"
+main_character_surname = "Baggins"
+main_character_age = "160"
+main_character_home ="The Shire"
+
+wizards_name = "Gandalf"
+wizards_age = "3000"
+
+villain_name ="Salron"
+villan_home = "Mordor"
+
+story = "Their once was a Hobbit who lived in "  + main_character_home + ".\nHis name was " + main_character_name + " " + main_character_surname +".\
+    \n" + main_character_name + " Was " + main_character_age + " years old today, which was unusually old for this Hobbit.\n\
+One day as he was sitting outside his home smoking his pipe, he had an unespected visit.\n\
+\nIt was " + wizards_name + "! He has a quest for " + main_character_name + ". He was to go to " + villan_home + " and\n\
+defeat " + villain_name + ".\n\
+\n\tThe End."
+
+
+print(story)
